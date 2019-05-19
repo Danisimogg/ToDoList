7cf11778c95cc4600f79a325a1067fdfa9f653c5
Danisimo <angrydaniall123456@gmail.com>
2019-05-20 00:27:29 +0300

    Add filter all/active/complete

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,8 @@ import TodoForm from "./TodoForm";
 import Todo from "./Todo";
 export default class TodoList extends React.Component {
         state = {
-            todos: []
+            todos: [],
+            todoToShow: "all"
         };
 
         addTodo = (todo) => {
@@ -11,34 +12,54 @@ export default class TodoList extends React.Component {
                 todos: [todo, ...this.state.todos]
             });
         };
-        toggleComplete = (id) =>{
-this.setState({
-    todos: this.state.todos.map(todo=>{
-        if (todo.id === id) {
-            //suppose to update
-            return {
-                ...todo,
-                complete: !todo.complete
-                // id:todo.id,
-                // text : todo.text,
-                // complete: !todo.complete
-            };
-        }else{
-            return todo;
-        }
-    })
-})
+        toggleComplete = (id) => {
+            this.setState({
+                todos: this.state.todos.map(todo => {
+                    if (todo.id === id) {
+                        //suppose to update
+                        return {
+                            ...todo,
+                            complete: !todo.complete
+                            // id:todo.id,
+                            // text : todo.text,
+                            // complete: !todo.complete
+                        };
+                    } else {
+                        return todo;
+                    }
+                })
+            });
+        };
+        updateTodoToShow = (s) => {
+            this.setState({
+                todoToShow: s
+            })
         }
     render(){
+let todos = [];
+
+if(this.state.todoToShow === "all"){
+    todos = this.state.todos;
+} else if (this.state.todoToShow === "active"){
+    todos = this.state.todos.filter(todo => !todo.complete);
+}else if (this.state.todoToShow === "complete"){
+    todos = this.state.todos.filter(todo => todo.complete);
+}
+
         return (
         <div>
             <TodoForm onSubmit={this.addTodo} />
-            {this.state.todos.map(todo => (
+            {todos.map(todo => (
                 <Todo key={todo.id} 
                 toggleComplete={() => this.toggleComplete(todo.id)}
                 todo = {todo} />
             ))}
             <div> todos left : {this.state.todos.filter(todo => !todo.complete).length}</div>
+            <div>
+               <button onClick={() => this.updateTodoToShow("all")}>all</button>
+               <button onClick={() => this.updateTodoToShow("active")}>active</button>
+               <button onClick={() => this.updateTodoToShow("complete")}>complete</button>
+            </div>
             </div>
         );
     }
